7bbdfe705954d7fe1362a2ab8c2bee0c5f1d1609
cdunahee36 <dunahee10@gmail.com>
2021-02-09 18:40:16 -0600

    Added more to the about page.

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,14 @@
 function About() {
   return (
-    <div>
-      
+    <div class="about-body">
+      <div class="about-box">
+        <h4 class="about-text"><span>This web application was developed by a person who loves 
+          WWII history and all the army tanks used within the war. The application allows the
+          user to search for the desired tank and read all the information provieded, plus a
+          photo of the chosen tank!
+          </span>
+        </h4>
+      </div>
     </div>
   );
 }
