0fe4823344636f2c18aee565100e3a42e28ca13d
cdunahee36 <dunahee10@gmail.com>
2021-02-10 20:49:28 -0600

    Added some  styling to the about component.

--- src/components/About.js
+++ src/components/About.js
@@ -2,10 +2,11 @@ function About() {
   return (
     <div class="about-body">
       <div class="about-box">
-        <h4 class="about-text"><span>This web application was developed by a person who loves 
-          WWII history and all the army tanks used within the war. The application allows the
-          user to search for the desired tank and read all the information provieded, plus a
-          photo of the chosen tank!
+        <h4 class="about-text"><span><p class="span-text">This database was created for you, to learn something you didnt already know!
+        The application lets you pick and choose through a default list of tanks, or you can use the search bar to find a specific tank.
+        Everything you see on this database was handcrafted and created from scratch. All of the information is correct and should be up 
+        to date. If you find an error please contact me and I will try my best to correct it. -Caleb Dunahee, Thanks!
+          </p>
           </span>
         </h4>
       </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <div>
-      <h3 class="home-title"><span>Welcome to the Tank Database!</span></h3>
+      <h3 class="home-title"><span><p class="span-text1">Welcome to the Tank Database!</p></span></h3>
     </div>
   )
 };
